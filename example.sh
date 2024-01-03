6926cbe6a0c682e74113edbda18577c92fec8645
Andres Lamilla <aflamillac@gmail.com>
2024-01-03 09:19:49 +0100

    Example updated

--- example.sh
+++ example.sh
@@ -5,19 +5,24 @@ PASSWORD=$2
 for i in {1..1000}
 do
   data='{
-      "service": "metric.test",
+      "service": "api.response_time",
       "description": "Example metric",
       "metric": '$(( $RANDOM % 10 ))',
       "host": "www.example.com",
       "ttl": 60,
       "state": "ok",
       "attributes": {
-          "hola": "mundo"
+        "ip": "152.206.232.215",
+        "statusCode": "200",
+        "method": "POST",
+        "url": "/api/countries",
+        "country": "CU"
       }
   }'
+
   #echo curl -d "$data" https://monitor.tropipay.com/metric
   #curl -u $USER:$PASSWORD -d "$data" https://monitor.tropipay.com/metric
   echo curl -d "$data" 127.0.0.1:8080/metric
-  curl -u user:password -d "$data" 127.0.0.1:8080/metric
+  curl -u user:pass -d "$data" 127.0.0.1:8080/metric
   sleep 1
 done
